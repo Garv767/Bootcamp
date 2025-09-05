dabf4a1fa2073a9dd1558fc3cc103325ed015027
Garv767 <garvrahut@gmail.com>
2025-09-05 18:25:49 +0530

    Update

--- Bootcamp3.0/main.java
+++
@@ -1,9 +0,0 @@
-class main {
-    public static void main(String[] args) {
-        String str = "Hello, World!";
-        String S1 = new String("hell\nno");
-
-        System.out.println(str+"\n"+S1);
-        
-    }
-}
